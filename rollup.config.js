eba96a1c03d67752808d809938970c77ec34ea05
Luca Haneklau <git@luca.lsys.ac>
2023-04-20 20:34:43 +0200

    fix html import

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import html from '@web/rollup-plugin-html';
+import {rollupPluginHTML} from '@web/rollup-plugin-html';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
@@ -6,5 +6,5 @@ import json from '@rollup/plugin-json';
 export default {
   input: './packages/index.html',
   output: { dir: 'dist' },
-  plugins: [html(), json(), resolve(), commonjs()],
+  plugins: [rollupPluginHTML(), json(), resolve(), commonjs()],
 };
